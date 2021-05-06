cb1bdb606e9c521ef0795562d7452c7ae5e95fb2
Arturo Prieto Tirado <52428590+apt345@users.noreply.github.com>
2021-05-06 13:15:33 +0200

    Final preprocessing version

--- preprocessing.R
+++ preprocessing.R
@@ -14,8 +14,11 @@ library(mice)
 ###################### Preprocessing ########################################
 
 
+#load dataset
 data = read.csv("C:/Users/arpri/OneDrive/Escritorio/libros/master/4- Cuarto Semicuatrimestre/Regresión Avanzada y Predicción/owid-covid-data.csv")
 
+
+# discard unwanted locations
 discard_locations=c("Anguilla","Northern Cyprus","Vatican","World","Turks and Caicos Islands"
 ,"Trinidad and Tobago","Timor","Togo","Saint Kitts and Nevis","Saint Vincent and the Grenadines"
 ,"San Marino","Saint Helena","Saint Lucia","Vanuatu","Solomon Islands"
@@ -27,14 +30,11 @@ discard_locations=c("Anguilla","Northern Cyprus","Vatican","World","Turks and Ca
 ,"Bermuda","Dominica","Kosovo","Gibraltar","Andorra","Sao Tome and Principe")
 
 data=dplyr::filter(data, !(data$location %in% discard_locations))
-#anguilla es una isla de 15000 personas, no interesa
-#borrar todos los micro estados, islas aisladas del mundo, continentes enteros y mundo
 
 
 
 
-#completar datos de paises que puedan tener na en gdp, population density,
-#median age, aged 65 or older, life expectancy, HDI
+#fill known missing values with google
 data$location[(is.na(data$population_density))]
 data$population_density[data$location=="Taiwan"]=656
 
@@ -58,7 +58,7 @@ data$human_development_index[data$location=="Taiwan"]=0.916
 data$human_development_index[data$location=="Macao"]=0.914
 
 
-#nueva variable is_island? las islas tienen mucho mejor control
+#new variable is island
 
 data$is_island="no"
 islands=c("Taiwan", "Macao", "Greenland", "United Kingdom", "Sri Lanka",
@@ -68,34 +68,7 @@ islands=c("Taiwan", "Macao", "Greenland", "United Kingdom", "Sri Lanka",
 data$is_island[data$location %in% islands]="yes"
 data$is_island=as.factor(data$is_island)
 
-#Erase Country, ISO code, location but leave continet because its a factor with not too
-#many levels that can account for geographical features and different seasons of the year in the same date, important.
-#Predict weekly icu admissions per million
-#smoothed son las medias a 7 dias, usarlas para captar mejor el efecto temporal
-#como estamos comparando paises, usar solo magnitudes normalizadas.
-#hospital beds per thousand esta logicamente relacionada con admisiones al hospital, quitarla
-#aged 70 older contenido en 65 older así como median age
-#population es mejor population density
-#como quiero la evolucion, quitar los cumulativos
-
-
-#Diferencia entre total vaccinated, people vaccinated etc???? Dejar solo una de las 
-#tres vacunas totales (o la parcial y el total)
-#total vaccinations, numero total de vacunas--> No tan interesante como las otras dos
-#people vaccinated: al menos una dosis-->reduce riesgo uci?? Interesante
-#people fully vaccinated: inmunes se supone, interesante
-#quitar pacientes de uci pues queremos prededir pacientes a las siete semanas
-# todos los weekly uci son NA... quitarlas y predecir otra cosa
-#volver a poner entonces icu_patients_per million, no porque hay 70000 NAs
-#tests per casees lo mismo que positive rate mas o menos
-#extreme poverty es similar a gdp per capita y tiene 30000 NAs, quitarla
-#predecir muertes
-#quitar new test per thousand
-#quitar todos los que tengan muchos NA
-#weekly hosp admissions per million y hosp_patients_per_million tiene 80000 NAs
-# las vacunas tambien pero esta claro que son 0 cuando hay NA, ponerlas a 0
-#quitar new vaccinations smoothed porque eso ya va en el total de vacunas
-
+# erase unwanted variables
 data = subset(data, select = -c(iso_code, location, total_cases, total_cases_per_million, new_cases,
       new_cases_smoothed, total_deaths, total_deaths_per_million, new_deaths, new_deaths_smoothed,
       new_cases_per_million, new_deaths_per_million, icu_patients, 
@@ -113,14 +86,11 @@ data = subset(data, select = -c(iso_code, location, total_cases, total_cases_per
 
 #data from July 1st is more reliable
 data=data[data$date>"2020-07-01",]
-#Leave +65, median age, and GDP as continuous (O AGRUPARLO??).
-#escalar variables para que el algoritmo no se descontrole con numeros grandes.
 
-#revisar que esto no este metiendo NA como 0 donde no debería
-#data$people_vaccinated_per_hundred[is.na(data$people_vaccinated_per_hundred)]=0
+# substitute Na with 0
 data$people_fully_vaccinated_per_hundred[is.na(data$people_fully_vaccinated_per_hundred)]=0
 
-#Cómo codificar el tiempo??? meses y factores
+#generate months
 
 month_fun=function(x){
    return(strsplit(x=x, split="-")[[1]][2])
@@ -150,12 +120,7 @@ testData = data[-trainRowNumbers, ]
 rm(trainRowNumbers)
 
 
-#una vez esta todo codificado, replace missing values with random forest
-# para train y test por separado para evitar data leakage
-#casos, muertes en fechas sin datos. Stringency index solo en algunas fechas
-#diabetes, riesgo cardiovascular 
-
-#filtrar fechas iniciales sobre todo para new cases smoothed y new deaths??
+#replace missing values with random forest
 
 set.seed(10)
 c3 <- makePSOCKcluster(7)
